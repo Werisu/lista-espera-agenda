5f513cb7c1fc7355dac140c1e308f602c0720a53
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-11 14:50:06 -0300

    refactor(layout): add links header component

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -2,22 +2,40 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
+const mockTitle = 'Agenda de Amigos';
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.title = mockTitle;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain title', () => {
+    const header: HTMLHeadingElement =
+      fixture.nativeElement.querySelector('.title');
+    expect(header.textContent).toBe(mockTitle);
+
+    component.title = 'New title';
+    fixture.detectChanges();
+    expect(header.textContent).toBe('New title');
+  });
+
+  it('should redirect to home when title is clicked', () => {
+    const anchor: HTMLAnchorElement =
+      fixture.nativeElement.querySelector('.title-link');
+    expect(anchor.getAttribute('href')).toBe('/');
   });
 });
--- modules/feature/layout/src/lib/header/header.component.ts
+++ modules/feature/layout/src/lib/header/header.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-header',
+  // eslint-disable-next-line @angular-eslint/prefer-standalone
   standalone: false,
-  
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  @Input({ required: true }) title = '';
 }
