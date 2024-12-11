4c654f5216bf90f1989867efe5c0bf308a73ce39
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-11 17:54:58 -0300

    test(friend-search): add tests

--- modules/feature/friend/search/src/lib/friend-search/friend-search.component.spec.ts
+++ modules/feature/friend/search/src/lib/friend-search/friend-search.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {
   FriendSearchService,
@@ -10,6 +15,7 @@ import { FriendSearchComponent } from './friend-search.component';
 describe('FriendSearchComponent', () => {
   let component: FriendSearchComponent;
   let fixture: ComponentFixture<FriendSearchComponent>;
+  let friendSearchService: FriendSearchService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,6 +28,7 @@ describe('FriendSearchComponent', () => {
       ],
     }).compileComponents();
 
+    friendSearchService = TestBed.inject(FriendSearchService);
     fixture = TestBed.createComponent(FriendSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -29,5 +36,86 @@ describe('FriendSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should debounce when input field is changed', fakeAsync(() => {
+    // ARRANGE
+    jest.spyOn(friendSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    // ACT
+    input.value = 'Alonzo';
+    input.dispatchEvent(new Event('input'));
+    // ASSERT
+    expect(friendSearchService.searchByName).not.toHaveBeenCalled();
+
+    tick(500);
+
+    expect(friendSearchService.searchByName).toHaveBeenCalledWith(input.value);
+  }));
+
+  it('should search multiple times', fakeAsync(() => {
+    // ARRANGE
+    jest.spyOn(friendSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    // ACT
+    input.value = 'Sherri';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    input.value = 'Alonzo';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    // ASSERT
+    expect(friendSearchService.searchByName).toHaveBeenCalledTimes(2);
+  }));
+
+  it('should prevent identical submissions', fakeAsync(() => {
+    // ARRANGE
+    jest.spyOn(friendSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    // ACT
+    input.value = 'Alonzo';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    input.value = 'Alonzo';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    // ASSERT
+    expect(friendSearchService.searchByName).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should prevent empty submissions', fakeAsync(() => {
+    // ARRANGE
+    jest.spyOn(friendSearchService, 'searchByName');
+    const input: HTMLInputElement =
+      fixture.nativeElement.querySelector('input');
+
+    // ACT
+    input.value = '';
+    input.dispatchEvent(new Event('input'));
+
+    tick(500);
+
+    // ASSERT
+    expect(friendSearchService.searchByName).not.toHaveBeenCalled();
+  }));
+
+  it('should return friend observable correctly', () => {
+    component.friends$.subscribe((result) =>
+      expect(result).toEqual(mockFriends)
+    );
   });
 });
