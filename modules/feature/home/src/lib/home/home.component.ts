d460d90bbc0d5c591129421175151e1f0cc0f9c5
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-12 11:42:03 -0300

    feat(home): use top friends service

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { mockFriends } from '@lista-espera-agenda/friend-data-access';
+import { TopFriendsService } from '@lista-espera-agenda/friend-data-access';
 
 @Component({
   selector: 'lib-home',
@@ -9,5 +9,9 @@ import { mockFriends } from '@lista-espera-agenda/friend-data-access';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  friends = mockFriends;
+  friends$;
+
+  constructor(private topFriendsService: TopFriendsService) {
+    this.friends$ = this.topFriendsService.getFriends();
+  }
 }
