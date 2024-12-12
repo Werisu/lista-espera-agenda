15cf4347312b8079b23f125e213628936f43e348
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-12 11:51:21 -0300

    feat(friend-ui): add lib

---
+++ modules/ui/friend/src/lib/components/friend-card/friend-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-friend-card',
+  imports: [CommonModule],
+  templateUrl: './friend-card.component.html',
+  styleUrl: './friend-card.component.scss',
+})
+export class FriendCardComponent {}
