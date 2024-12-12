b4e55a869a8cd7d218646fd085ffe0279ab7bdcc
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2024-12-12 13:54:44 -0300

    feat(friend-detail): add lib

---
+++ modules/feature/friend/detail/src/lib/friend-detail/friend-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-friend-detail',
+  imports: [CommonModule],
+  templateUrl: './friend-detail.component.html',
+  styleUrl: './friend-detail.component.scss',
+})
+export class FriendDetailComponent {}
